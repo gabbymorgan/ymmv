bae01f13b223128939475952f508ebc09928eafd
William Pelton <williamrpelton@gmail.com>
2018-07-12 18:17:34 -0500

    finished login modal

---
+++ src/components/FoodCard/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FoodItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default FoodItem;
